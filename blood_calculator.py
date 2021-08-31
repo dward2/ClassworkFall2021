d7cb152a27d6ac0b3d5845e647c5f27be0a2c227
David Ward <david.a.ward@duke.edu>
2021-08-31 11:19:35 -0400

    Add LDL analysis option

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,17 +4,17 @@ def interface():
     while keep_running:
         print("Make a choice")
         print("1 - HDL Analysis")
+        print("2 - LDL Analysis")
         print("9 - Quit")
         choice = int(input("Make a choice: "))
-        print(type(choice))
+        # print(type(choice))
         if choice == 9:
             keep_running = False
         elif choice == 1:
             HDL_Driver()
+        elif choice == 2:
+            LDL_Driver()
 
-
-    print(choice)
-    return choice
     
 def HDL_Driver():
     HDL_value = hdl_input()
@@ -37,6 +37,29 @@ def hdl_output(HDL_value, HDL_answer):
     print("The HDL value of {} is considered {}".format(HDL_value, HDL_answer))
     return
 
+
+def LDL_Driver():
+    LDL_value = ldl_input()
+    LDL_character = ldl_analysis(LDL_value)
+    ldl_output(LDL_value, LDL_character)
+    
+def ldl_input():
+    ldl_value = int(input(("Enter LDL Value: ")))
+    return ldl_value
+    
+def ldl_analysis(LDL_value):
+    if LDL_value < 130:
+        return "Normal"
+    elif 130 <= LDL_value < 160:
+        return "Borderline High"
+    elif 160 <= LDL_value < 190:
+        return "High"
+    else:
+        return "Very High"
+        
+def ldl_output(LDL_value, LDL_answer):
+    print("The LDL value of {} is considered {}".format(LDL_value, LDL_answer))
+    return
     
     
 interface()
