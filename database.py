c382c446573de68939c80458df7f01d51e3d952d
David Ward <david.a.ward@duke.edu>
2021-09-20 21:28:06 -0400

    Updating patient record to be a Class instance

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
+class Patient:
+
+    def __init__(self, input_name, id_no, age):
+        self.name = input_name
+        self.id_no = id_no
+        self.age = age
+        self.tests = []
+
+        
+    def __repr__(self):
+        return "{}: {}".format(self.id_no, self.name)
+        
+    def is_adult(self):
+        if self.age >= 21:
+            return True
+        else:
+            return False
+        
+
 def create_database_entry(patient_name, id_no, age):
-    new_patient = {"name": patient_name, "id_no": id_no, 
-                   "age": age, "tests": []}
+    new_patient = Patient(patient_name, id_no, age)
+    #new_patient = {"name": patient_name, "id_no": id_no, 
+    #               "age": age, "tests": []}
     #new_patient = [patient_name, id_no, age, []]
     return new_patient
 
@@ -14,7 +34,7 @@ def print_database(db):
 
 def print_patients_over_age(age, db):
     for patient in db:
-        if patient[2] > age:
+        if patient["age"] > age:
             print(patient[0])
 
 
@@ -28,21 +48,22 @@ def get_patient(db, id_no):
 def main():
     db = {}
     x = create_database_entry("Ann Ables", 120, 30)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("Bob Boyles", 24, 31)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("Chris Chou", 33, 33)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("David Dinkins", 14, 34)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     print(db)
 
     patient_id_tested = 24
     test_done = ("HDL", 65)
 
     patient = get_patient(db, patient_id_tested)
-    patient["tests"].append(test_done)
-    print(db)
+    patient.tests.append(test_done)
+    print(patient.is_adult())
+    print(db[24].tests)
     
     print_database(db)
 
