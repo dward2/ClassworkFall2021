53cd442a903352c937f23d17c2d4193b9d0174b7
David Ward <david.a.ward@duke.edu>
2021-09-27 16:48:18 -0400

    Fix pep8 in fibinaci.py

--- fibinaci.py
+++ fibinaci.py
@@ -2,13 +2,13 @@ import logging
 
 
 def fibinaci(input_list):
-   next_entry = input_list[-2] + input_list[-1]
-   input_list.append(next_entry)
-   logging.info("This iteration is {}".format(input_list))
-   if next_entry <= 100:
-       fibinaci(input_list)
-   else:
-       logging.warning("This is the last iteration.")
+    next_entry = input_list[-2] + input_list[-1]
+    input_list.append(next_entry)
+    logging.info("This iteration is {}".format(input_list))
+    if next_entry <= 100:
+        fibinaci(input_list)
+    else:
+        logging.warning("This is the last iteration.")
 
 
 if __name__ == "__main__":
