f5df5f71464f4223eb880c2b8238af7f6337bf9e
David Ward <david.a.ward@duke.edu>
2021-11-04 15:40:44 -0400

    Completed window design and connected to code in 11/3/2021 class

--- health_db_gui.py
+++ health_db_gui.py
@@ -1,29 +1,87 @@
 import tkinter as tk
 from tkinter import ttk
 
+from health_db_client import add_patient_to_server
 
-def create_output(name, id, blood_letter, rh_factor):
+
+def create_output(name, id, blood_letter, rh_factor, center):
+    """Interface between GUI and server
+
+    This function is call by the GUI command function attached to the "Ok"
+    button of the GUI.  As input, it takes the data entered into the GUI.
+    It creates an output string that is sent back to the GUI that is
+    printed in the console.  It also call a function in the client module
+    that will send the data to the server.  It returns the response that
+    is received from the server
+
+    Args:
+        name (str): patient name entered in GUI
+        id (str): patient id (medical record number) entered in GUI
+        blood_letter (str):  patient blood type entered in GUI
+        rh_factor (str):  patient rh_factor entered in GUI
+        center (str): nearest donation center entered in GUI
+
+    Returns:
+        str, str: a formatted string containing patient information from the
+            GUI, the response from the server when adding the patient
+
+    """
     out_string = "Patient name: {}\n".format(name)
     out_string += "Blood type: {}{}\n".format(blood_letter, rh_factor)
-    return out_string
+    out_string += "Donation Center: {}\n".format(center)
+    answer = add_patient_to_server(name, id,
+                                   "{}{}".format(blood_letter, rh_factor))
+    return out_string, answer
 
 
 def design_window():
+    """Creates the main GUI window for the health database
+
+    A GUI window is created that is the main interface for the health
+    database.  It accepts information from the user (patient name, id,
+    blood type, rh factor, and nearest donation center).  Upon hitting the
+    Ok button, this information is sent to the server.  Upon hitting the
+    Cancel button, the window closes.
+
+    Returns: None
+
+    """
 
     def ok_button_cmd():
+        """Event to run when Ok button is pressed
+
+        This function is connected to the "Ok" button of the GUI.  It follows
+        the typical pattern for these command functions:
+        1. It gets the needed information from the GUI.
+        2. It calls a function external to the GUI to process the data and
+        receive the results
+        3. It updates the GUI based on the received results.  In this case,
+        that includes printing to the console and updating a Label in the GUI.
+
+        Returns: None
+
+        """
         # Get needed data from GUI
         name = name_data.get()
         id = id_data.get()
         blood_letter = blood_letter_data.get()
         rh_factor = rh_data.get()
+        center = donation_center_data.get()
 
         # Call external function to do the work that can be tested
-        answer = create_output(name, id, blood_letter, rh_factor)
+        out_string, answer = create_output(name, id, blood_letter, rh_factor,
+                                           center)
 
         # Update GUI
-        print(answer)
+        print(out_string)
+        output_string.configure(text=answer)
 
     def cancel_cmd():
+        """Closes window upon click of Cancel button
+
+        This function is connected to the "Cancel" button of the GUI.  It
+        destroys the root window causing the GUI interface to close.
+        """
         root.destroy()
 
     root = tk.Tk()
@@ -31,19 +89,19 @@ def design_window():
     # root.geometry("10x2")
 
     top_label = ttk.Label(root, text="Blood Donor Database")
-    top_label.grid(column=0)
+    top_label.grid(column=0, row=0, columnspan=2, sticky='w')
 
     ttk.Label(root, text="Name").grid(column=0, row=1, sticky='e')
 
     name_data = tk.StringVar()
-    name_entry_box = ttk.Entry(root, width=50, textvariable=name_data)
-    name_entry_box.grid(column=1, row=1)
+    name_entry_box = ttk.Entry(root, width=40, textvariable=name_data)
+    name_entry_box.grid(column=1, row=1, sticky='w', columnspan=2)
 
     ttk.Label(root, text="ID").grid(column=0, row=2)
 
     id_data = tk.StringVar()
     id_entry_box = ttk.Entry(root, width=10, textvariable=id_data)
-    id_entry_box.grid(column=1, row=2)
+    id_entry_box.grid(column=1, row=2, sticky='w')
 
     blood_letter_data = tk.StringVar()
     ttk.Radiobutton(root, text='A', variable=blood_letter_data,
@@ -56,10 +114,23 @@ def design_window():
                     value='O').grid(column=0, row=6, sticky=tk.W)
 
     rh_data = tk.StringVar()
+    rh_data.set('-')
     rh_checkbox = ttk.Checkbutton(root, text="Rh Positive",
                                   variable=rh_data, onvalue='+',
                                   offvalue='-')
     rh_checkbox.grid(column=1, row=4)
+
+    ttk.Label(root, text="Nearest Donation Center").grid(column=3, row=0)
+
+    donation_center_data = tk.StringVar()
+    combo_box = ttk.Combobox(root, textvariable=donation_center_data)
+    combo_box["values"] = ("Durham", "Raleigh", "Cary", "Apex")
+    combo_box.state(["readonly"])
+    combo_box.grid(column=3, row=1)
+
+    # This output_string will be used to display results from the server
+    output_string = ttk.Label(root)
+    output_string.grid(column=0, row=10)
 
     ok_button = ttk.Button(root, text="Ok", command=ok_button_cmd)
     ok_button.grid(column=1, row=6)
